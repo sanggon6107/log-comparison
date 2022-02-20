83133725d430cb95a53d41be942d6b3d272cc8c1
sanggon6107 <sanggon6107@naver.com>
2022-02-20 17:10:50 +0900

    함수 이름 변경

--- Ilog.cpp
+++ Ilog.cpp
@@ -1,7 +1,7 @@
 #include "Ilog.h"
 #include "include.h"
 
-void Ilog::merge(Ilog& log_1, Ilog& log_2)
+void Ilog::comparison(Ilog& log_1, Ilog& log_2) // comparison으로 이름 변경 필요.
 {
 	// 머지. 
 }
--- Ilog.h
+++ Ilog.h
@@ -5,7 +5,7 @@ class Ilog
 {
 public :
 	virtual ~Ilog();
-	void merge(Ilog& log_1, Ilog& log_2);
+	void comparison(Ilog& log_1, Ilog& log_2);
 	
 private :
 	Table table_;
